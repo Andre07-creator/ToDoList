287a272d2975d5fb3ce41222a68a243d6ec73251
Andre07-creator <aramaldes876@gmail.com>
2022-08-09 20:45:57 -0300

    chamada com o express, usando o postman

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const express = require('express')
+const app = express();
+app.get('/json', (req, res)=>{
+    res.json({title: "Tarefas", done: true})
+})
+app.get('/', (req,res)=>{
+    res.send("<h1>Minha lista de tarefas</h1>")
+})
+app.get('/planet',(req, res)=>{
+    res.send("<h1>Jupiter</h1> </hr> <p>Júpiter é o maior planeta do Sistema Solar, tanto em diâmetro quanto em massa, e é o quinto mais próximo do Sol.[11] Possui menos de um milésimo da massa solar, contudo tem 2,5 vezes a massa de todos os outros planetas em conjunto.</p>")
+})
+app.listen(3000, ()=>{
+    console.log("O servidor foi iniciado!")
+})
